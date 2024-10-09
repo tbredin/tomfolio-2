a540f171fe0c4a18f1175d76aab8b39d831684f8
tbredin <tbredin@gmail.com>
2024-10-09 18:45:55 +1100

    Add more entry points

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        404: resolve(__dirname, '404.html'),
+        cv: resolve(__dirname, 'cv.html'),
+      },
+    },
+  },
+})
